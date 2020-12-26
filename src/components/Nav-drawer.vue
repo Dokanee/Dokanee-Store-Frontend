cc59b5163691504d838076aca9a050af902d0fb5
Injamamul Haque Sonet <ihsonnet@gmail.com>
2020-12-27 02:19:07 +0600

    set in router

---
+++ src/components/Nav-drawer.vue
@@ -0,0 +1,44 @@
+<template v-slot:prepend>
+  <div>
+    <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/women/81.jpg">
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Injamamul Haque Sonet</v-list-item-title>
+            <v-list-item-subtitle>Store Owner</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+      <v-divider></v-divider>
+      
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+        >
+          <v-list-item-icon>
+            <v-icon size="18">{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          { title: 'All Products', icon: 'mdi-home-city' },
+          { title: 'My Account', icon: 'mdi-account' },
+          { title: 'Users', icon: 'mdi-account-group-outline' },
+        ],
+      }
+    },
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      clipped
+      app
+    >
+    <Nav-drawer></Nav-drawer>
+    
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <span class="title ml-3 mr-8"><span style="color:#00AC9C"><b>Dokanee&nbsp;</b></span><span class="font-weight-light">Store</span></span>
+      <v-text-field
+        solo-inverted
+        flat
+        hide-details
+        label="Search"
+        prepend-inner-icon="mdi-search"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-icon class="ma-4" color="red lighten-2">mdi-cart</v-icon>
+      <v-avatar>
+      <img
+        src="https://cdn.vuetifyjs.com/images/john.jpg"
+        alt="John"
+      >
+    </v-avatar>
+    </v-app-bar>
+     <v-content clipped>
+      <v-container fluid>
+        Basic layout.
+      </v-container>
+    </v-content>
+    <!-- <v-footer color="#E7E7E7" app text-align-center>
+      <span class="justify-center black--text black--border">Copyright &copy; 2020 | Dokanee.com.bd</span>
+    </v-footer> -->
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navdrawer from "@/components/Nav-drawer.vue";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      "Nav-drawer": Navdrawer,
+    },
+    data: () => ({ drawer: null }),
   }
-}
 </script>
